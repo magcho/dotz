76587e065dfe59676a1dac6558ab163a511cd8e3
magcho <mail@magcho.com>
2019-09-11 23:21:26 +0900

    [update] toml perser

--- main.go
+++ main.go
@@ -2,15 +2,52 @@ package main
 
 import (
 	"fmt"
-	// "github.com/go-yaml/yaml"
+	"github.com/BurntSushi/toml"
 	"github.com/urfave/cli"
-	// "io/ioutil"
 	"os"
 	"os/exec"
 	"regexp"
 	"strings"
-  "time"
+	"time"
+  "bytes"
+  "io/ioutil"
+  // "reflect"
 )
+
+// yamlから読み込むための構造体を定義
+
+type DotzConfigToml struct {
+	Tracked tracked
+}
+type tracked struct{
+  Files [][]interface{}
+}
+
+func ReadDotzConf(fileName string) (config DotzConfigToml){
+  var configs DotzConfigToml
+  _, err := toml.DecodeFile(fileName, &configs)
+  if err != nil{
+    fmt.Println(err)
+  }
+
+  return configs
+}
+
+func WriteDotzConf(config DotzConfigToml, filename string){
+   if !FileExists(filename){
+    return
+  }
+
+  var buff bytes.Buffer
+  if err := toml.NewEncoder(&buff).Encode(config); err != nil{
+    fmt.Println(err)
+  }
+  writeBuff := []byte(buff.String())
+  err := ioutil.WriteFile(filename, writeBuff, 0644)
+  if err != nil {
+    fmt.Println(err)
+  }
+}
 
 func FolderExists(filename string) bool {
 	if f, err := os.Stat(filename); os.IsNotExist(err) || !f.IsDir() {
@@ -34,6 +71,7 @@ func FileExists(filename string) bool {
 
 func ParseFilePath(path string) (string, string) {
 	r := regexp.MustCompile(`\s*/\s*`)
+
 	result := r.Split(path, -1)
 	fileName := result[len(result)-1]
 	dirPath := strings.Join(result[:len(result)-2], "/") + "/"
@@ -43,7 +81,7 @@ func ParseFilePath(path string) (string, string) {
 func main() {
 	var (
 		DOTZ_ROOT string
-		home      string
+		HOME      string
 	)
 
 	app := cli.NewApp()
@@ -68,8 +106,8 @@ func main() {
 		},
 		cli.StringFlag{
 			// 環境変数からの引き継ぎ
-			Name:        "home",
-			Destination: &home,
+			Name:        "HOME",
+			Destination: &HOME,
 			EnvVar:      "HOME",
 		},
 	}
@@ -123,35 +161,33 @@ func main() {
 			Name:    "backup",
 			Aliases: []string{"b"},
 			Usage:   "backup tracked files",
-      Flags: []cli.Flag{
-        cli.BoolFlag{
-          Name: "push, p",
-          Usage: "enable `git push`",
-        },
-      },
+			Flags: []cli.Flag{
+				cli.BoolFlag{
+					Name:  "push, p",
+					Usage: "enable `git push`",
+				},
+			},
 			Action: func(c *cli.Context) error {
 
-        out, _ := exec.Command("git", "-C", DOTZ_ROOT, "status", "-z").Output()
-        
+				out, _ := exec.Command("git", "-C", DOTZ_ROOT, "status", "-z").Output()
 
-        if len(out) != 0 {
-          // stageに変更があるとき
-          exec.Command("git", "-C", DOTZ_ROOT, "add", "-A").Run()
+				if len(out) != 0 {
+					// stageに変更があるとき
+					exec.Command("git", "-C", DOTZ_ROOT, "add", "-A").Run()
 
-          commitMessage := "[dotz][backup] " + time.Now().Format("2006-01-02 15:04'05")
-          fmt.Println(commitMessage)
-          exec.Command("git", "-C", DOTZ_ROOT, "commit", "-m", commitMessage).Run()
+					commitMessage := "[dotz][backup] " + time.Now().Format("2006-01-02 15:04'05")
+					fmt.Println(commitMessage)
+					exec.Command("git", "-C", DOTZ_ROOT, "commit", "-m", commitMessage).Run()
 
-        }else{
-          fmt.Println("No change")
-        }
+				} else {
+					fmt.Println("No change")
+				}
 
-        
-        if c.Bool("push"){
-          exec.Command("git", "-C", DOTZ_ROOT, "push").Run()
-          fmt.Println("pushed")
-        }
-				
+				if c.Bool("push") {
+					exec.Command("git", "-C", DOTZ_ROOT, "push").Run()
+					fmt.Println("pushed")
+				}
+
 				return nil
 			},
 		},
@@ -183,17 +219,11 @@ func main() {
 	}
 
 	app.Action = func(c *cli.Context) error {
-		// var out_ string
 
-		out, _ := exec.Command("pwd").Output()
-		fmt.Printf("%s\n", out)
+		config := ReadDotzConf("./dotzconfig.toml")
+		// fmt.Println(config)
 
-		out, _ = exec.Command("cd", "../").Output()
-		fmt.Printf("%s\n", out)
-
-		out, _ = exec.Command("cd", "../", ";", "pwd").Output()
-		fmt.Printf("%s\n", out)
-
+    WriteDotzConf(config, DOTZ_ROOT+"dotz.toml")
 		return nil
 	}
 
