da988578ad273ce504a30763f505ff4acf8a5d1d
magcho <mail@magcho.com>
2020-02-24 00:02:01 +0900

    [update]

--- .github/actions/homebrew_test/src/index.js
+++ .github/actions/homebrew_test/src/index.js
@@ -4,8 +4,33 @@ const exec = util.promisify(childProcess.exec);
 const core = require("@actions/core");
 
 async function main() {
-  await console.log("pwd");
-  await exec("sh .github/actions/homebrew_test/main.sh");
+  // await exec("sh .github/actions/homebrew_test/main.sh");
+  const formulaName = core
+    .getInput("formula_url")
+    .replace("https://github.com/", "")
+    .replace(/\/homebrew-/, "")
+    .replace(/\/$/, "")
+    .replace(/.git$/, "");
+  const formulaFilePath = core.getInput("formula_file_path");
+  const commandName = core.getInput("command_name");
+
+  await exec(
+    '/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
+  );
+  await exec(`brew tap ${formulaName}`);
+  await exec("brew update");
+  const formulaPath = await exec(`brew --repository ${formulaName}`);
+  await exec(`cp ${formulaFilePath} ${formulaPath}`);
+
+  await exec(`brew audit ${commandName} --fix`);
+  await exec(`brew style ${formulaFilePath} --fix`);
+
+  await exec(`brew install ${commandName}`);
+
+  const result = await exec(`which ${commandName}`).stdout.match("not found");
+  if (result == null) {
+    return Error("can not installed");
+  }
 }
 
 main().catch(err => core.setFailed(err.message));
